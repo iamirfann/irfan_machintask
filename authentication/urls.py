e03d0e812dd32dfc9091b5bdd2079391a211722a
iamirfann <iam.irfansheriff@gmail.com>
2021-09-26 02:50:01 +0530

    initial commit

---
+++ authentication/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('login/', views.LoginApiView.as_view(), name='login'),
+    path('register/', views.RegisterApiView.as_view(), name='register'),
+    path('logout/', views.logoutUser, name='logout'),
+]
---
+++ authentication/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+
+# from authn import serializers
+from django.shortcuts import render
+from rest_framework import viewsets
+from django.shortcuts import render, redirect
+# from .serializers import RegisterSerializer
+from django.contrib.auth import login, logout
+from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+# Create your views here.
+
+class RegisterApiView(APIView):
+
+    def post(self, request):
+        req = request.data
+        username = req['username']
+        email = req['email']
+        password = req['password']
+        role = req['role']
+        get_username = User.objects.filter(username=username).exists()
+        if get_username:
+            return Response({"error": "username already exists"})
+        elif int(role) == 1:
+            user = User.objects.create_superuser(username=username, email=email, password=password)
+            user.save()
+            user.set_password(password)
+            user.save()
+            return Response({'success': 'admin registered successfully'})
+        elif int(role) == 2:
+            user = User.objects.create_superuser(username=username, email=email, password=password)
+            user.save()
+            user.set_password(password)
+            user.save()
+            return Response({'success': 'user registered successfully'})
+        # print(data)
+        # return render(request, "authn/register.html")
+
+
+class LoginApiView(APIView):
+
+    def post(self, request):
+        req = request.data
+        username = req['username']
+        password = req['password']
+        get_user = User.objects.get(username=username)
+        get_user.check_password(password)
+
+        if get_user:
+            login(request=request, user=get_user)
+            return Response({'success': 'login successfull'})
+
+
+def logoutUser(request):
+    logout(request)
+    return Response({'success': 'logout successfull'})
+
+
+
