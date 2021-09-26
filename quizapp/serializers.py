f925fd3bc17017e3e074f91c8998fb37bcb40db8
iamirfann <iam.irfansheriff@gmail.com>
2021-09-26 17:26:08 +0530

    added serializer

--- quizapp/serializers.py
+++ quizapp/serializers.py
@@ -1,3 +1,8 @@
 from rest_framework import serializers
 from .models import Question, Answer
 
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
--- quizapp/urls.py
+++ quizapp/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import QuestionView, AnswerView, AnswerSummaryView
+
 urlpatterns = [
     path('addquestion/', QuestionView.as_view(), name='create_question'),
     path('answer/', AnswerView.as_view(), name='answer'),
